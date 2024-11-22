85704fc081a5623e2371da609fbf1bc9d84f63ef
Gimeitarou <300kyen@gmail.com>
2024-11-22 15:24:22 +0900

    Large Update

--- Taste_of_honey-pod.py
+++ Taste_of_honey-pod.py
@@ -3,7 +3,7 @@ import os
 import time
 import subprocess
 
-AutherSigniture = "Written by Gimeitarou."
+AutherSigniture = "Built by Gimeitarou."
 
 if os.path.isfile('Signiture.txt') == False: #When no Signiture.txt
     print("Let's look for your first Honey Pod.\n") #First greeting
@@ -23,16 +23,15 @@ f.close
 #check if Signiture.txt is overwritten or not
 if OverWritten != AutherSigniture:
     subprocess.run('color f0 ' #white
-                   +'& echo Found Honey Pod!! & timeout 5 > nul '
+                   +'& echo Honey Pod Found !! & timeout 5 > nul '
                    +'& color 0f '#black
-                   +'& echo Tasty or not?'
-                   +'& timeout 1 > nul'
-                   +'& echo Best wishes for your Red team life!'
+                   +"& echo Tasty, wasn't it?"
+                   +'& timeout 2 > nul'
+                   +'& echo Best wishes for your Blue team life.'
                    +'& timeout 3 > nul', shell=True)
 else:
-    print("Not Found.\n")
+    print("Honey Pod Not Found.\n")
     time.sleep(2)
-
 
 #Copyright (c) 2024 Gimeitarou
 #This software is released under the MIT License, see LICENSE.
