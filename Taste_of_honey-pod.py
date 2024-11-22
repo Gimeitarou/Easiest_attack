a41c297719c138b59d4ccee355ad65cafd1c370e
Gimeitarou <300kyen@gmail.com>
2024-11-22 15:05:22 +0900

    Large Update

--- Taste_of_honey-pod.py
+++ Taste_of_honey-pod.py
@@ -1,32 +1,38 @@
+import sys
 import os
 import time
 import subprocess
 
 AutherSigniture = "Written by Gimeitarou."
 
-print("Dear bears, look for your first honey-pod.\n") #First greeting
-
-#make Signiture.txt only when it doesn't exist
-if os.path.isfile('Signiture.txt') == False: #check if there's Signiture file or not
+if os.path.isfile('Signiture.txt') == False: #When no Signiture.txt
+    print("Let's look for your first Honey Pod.\n") #First greeting
+    time.sleep(2)
     f = open("Signiture.txt", 'w', encoding='UTF-8')
     f.write(f'{AutherSigniture}')
     f.close()
     print('Making signiture file...')
     time.sleep(3)
+    sys.exit()
 
-#put contents of TEXT.txt into OverWritten
+#put contents of Signiture.txt into OverWritten
 f = open("Signiture.txt", 'r', encoding='UTF-8')
 OverWritten = f.read()
 f.close
 
-#check if Signiture is overwritten in a specific way
+#check if Signiture.txt is overwritten or not
 if OverWritten != AutherSigniture:
     subprocess.run('color f0 ' #white
-                    +'& echo Found Honey-pod. & timeout 5 > nul '
-                    +'& color 0f '#black
-                    +'& echo Tasty or not?'
-                    +'& echo Happy Red team life, dear.'
-                    +'& timeout 3 > nul', shell=True)
+                   +'& echo Found Honey Pod!! & timeout 5 > nul '
+                   +'& color 0f '#black
+                   +'& echo Tasty or not?'
+                   +'& timeout 1 > nul'
+                   +'& echo Best wishes for your Red team life!'
+                   +'& timeout 3 > nul', shell=True)
+else:
+    print("Let's look for your first Honey Pod.\n")
+    time.sleep(2)
+
 
 #Copyright (c) 2024 Gimeitarou
 #This software is released under the MIT License, see LICENSE.
